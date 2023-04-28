2bc1f55ab8ccef71219ed295b705641db82ec926
jack <jackdek11@gmail.com>
2023-04-28 21:03:15 +0200

    Changed home pgae styles

--- hireling/src/App.vue
+++ hireling/src/App.vue
@@ -1,59 +1,23 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="w-full h-full">
+    <div class="custom-bg-color ">
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
+.custom-bg-color {
+  position: absolute;
+  width: 20vw;
+  bottom: 0px;
+  left: 0px;
+  height: 30vh;
+  background-color: var(--vt-c-powder);
+  border-radius: 1rem;
 }
 
 @media (min-width: 1024px) {
